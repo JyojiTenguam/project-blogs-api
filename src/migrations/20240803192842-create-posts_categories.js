c4d2185b752c8997e75d47c3c93ad30e8ade07f1
JyojiTenguam <j.y.oji@hotmail.com>
2024-08-04 11:56:35 -0300

    Resolvendo requisito 02 de Project blogs API

--- src/migrations/20240803192842-create-posts_categories.js
+++ src/migrations/20240803192842-create-posts_categories.js
@@ -4,8 +4,8 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('posts_categories', {
       post_id: {
-        type: Sequelize.INTEGER,
         field: 'post_id',
+        type: Sequelize.INTEGER,
         references: {
           model: 'blog_posts',
           key: 'id'
@@ -15,8 +15,8 @@ module.exports = {
         primaryKey: true,
       },
       category_id: {
-        type: Sequelize.INTEGER,
         field: 'category_id',
+        type: Sequelize.INTEGER,
         references: {
           model: 'categories',
           key: 'id'
---
+++ src/models/User.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    displayName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  }, {
+    tableName: 'users',
+    underscored: true,
+    timestamps: false,
+  });
+
+  return User;
+};
+
+module.exports = User;
